433ea3ae1a643b9dceed7c3c6ca3fa6bec0cc3d0
Anri Lombard <anri.m.lombard@gmail.com>
2021-12-29 13:29:14 +0200

    Progress in warmup

--- HackerRank/Algorithms/Warmup/5.cpp
+++ HackerRank/Algorithms/Warmup/5.cpp
@@ -2,14 +2,26 @@
 
 using namespace std;
 
-int diagonalDifference(vector<vector<int>> arr) {
-
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    
+    int n;
+    cin >> n;
+    int mat[n][n];
+
+    for (int i=0; i<n; i++) 
+        for (int j=0; j<n; j++)
+            cin >> mat[i][j];
+
+    int sum1=0, sum2=0;
+
+    for (int i=0; i<n; i++)
+        sum1 += mat[i][i];
+    for (int i=n-1; i>-1; i--)
+        sum2 += mat[n-i-1][i];
+
+    cout << abs(sum1 - sum2)<< "\n";
+
 }
---
+++ HackerRank/Algorithms/Warmup/6.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void plusMinus(vector<int> arr) {
+    float zero=0, pos=0, neg=0;
+
+    for (int i=0; i<arr.size(); i++){
+        if (arr[i] >0) pos++;
+        else if (arr[i] < 0) neg++;
+        else zero++;
+    }
+
+    int den = arr.size();
+    cout << setprecision(6) << fixed;
+    cout << pos/den << " " << neg/den << " " << zero/den;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector <int> arr(n);
+    for (int i=0; i<n; i++)
+        cin >> arr[i];
+
+    plusMinus(arr);
+
+}

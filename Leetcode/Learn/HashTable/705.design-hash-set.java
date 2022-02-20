fa82361fd9b409d67968d94db9c917a089706eb1
Anri Lombard <anri.m.lombard@gmail.com>
2022-02-20 06:38:25 +0200

    Better Hash set implementation

--- Leetcode/Learn/HashTable/705.design-hash-set.java
+++ Leetcode/Learn/HashTable/705.design-hash-set.java
@@ -7,26 +7,47 @@
 // @lc code=start
 class MyHashSet {
     /**
-     * Time: O(1)
+     * Time: O(1)/O(N)
      * Space: O(N)
      */
 
-    private boolean[] arr;
+    private final int MAX_VALUE = 1000000;
+    private final int ARRAY_SIZE = 100;
+    private List<List<Integer>> parentList;
 
     public MyHashSet() {
-        this.arr = new boolean[1000001];
+        parentList = new ArrayList<>(ARRAY_SIZE);
+        for (int i=0; i<ARRAY_SIZE; i++) {
+            parentList.add(null);
+        }
     }
     
     public void add(int key) {
-        arr[key] = true;
+        int index = key % ARRAY_SIZE;
+        List<Integer> childList = parentList.get(index);
+        if (childList == null) {
+            List<Integer> newList = new LinkedList<Integer>();
+            newList.add(key);
+            parentList.set(index, newList);
+        } else {
+            if (!childList.contains(key)) {
+                childList.add(key);
+            }
+        }
     }
     
     public void remove(int key) {
-        arr[key] = false;
+        int index = key % ARRAY_SIZE;
+        List<Integer> childList = parentList.get(index);
+        if (childList != null) {
+            childList.remove(Integer.valueOf(key));
+        }
     }
     
     public boolean contains(int key) {
-        return arr[key];
+        int index = key % ARRAY_SIZE;
+        List<Integer> childList = parentList.get(index);
+        return childList != null && childList.contains(key);
     }
 }
 

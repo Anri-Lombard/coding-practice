3ed44bce6b99a296a8afe0b43daa85cab6f8b9c1
Anri Lombard <anri.m.lombard@gmail.com>
2022-02-26 14:39:52 +0200

    Data structure progress

--- Leetcode/Learn/BinarySearch/744.find-smallest-letter-greater-than-target.java
+++ Leetcode/Learn/BinarySearch/744.find-smallest-letter-greater-than-target.java
@@ -7,7 +7,14 @@
 // @lc code=start
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        
+        int l=0, h=letters.length;
+
+        while (l < h) {
+            int m = l + (h-l) / 2;
+            if (letters[m] <= target) l = m + 1;
+            else h = m;
+        }
+        return letters[l % letters.length];
     }
 }
 // @lc code=end

a33f6bbc07317d2de25bc8fc5c4e78f58486a5b2
Anri Lombard <anri.m.lombard@gmail.com>
2022-02-28 06:51:37 +0200

    Interview progress

---
+++ Leetcode/Learn/TopInterviewQuestions/283.move-zeroes.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=283 lang=java
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        for (int num: nums) {
+            if (num != 0) nums[insertPos++] = num;
+        }        
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+}
+// @lc code=end
+

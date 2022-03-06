e8bec6415192d0a7cf3a158ae73dfe4b799219e4
Anri Lombard <anri.m.lombard@gmail.com>
2022-03-06 18:35:02 +0200

    Interview progress

---
+++ Leetcode/Learn/TopInterviewQuestions/98.validate-binary-search-tree.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=98 lang=java
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+        Integer prev = null;
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+
+            if (prev != null && root.val <= prev) {
+                return false;
+            }
+            prev = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
+// @lc code=end
+

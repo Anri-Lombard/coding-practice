ea24063598d71c9d60172212c87221072ccb1a4b
Anri Lombard <anri.m.lombard@gmail.com>
2022-03-10 16:10:39 +0200

    Interview Progress

---
+++ Leetcode/Learn/TopInterviewQuestions/198.house-robber.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=198 lang=java
+ *
+ * [198] House Robber
+ */
+
+// @lc code=start
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        int prev1 =0;
+        int prev2 =0;
+        for (int num: nums) {
+            int tmp = prev1;
+            prev1 = Math.max(prev2 + num, prev1);
+            prev2 = tmp;
+        }
+        return prev1;
+    }
+}
+// @lc code=end
+

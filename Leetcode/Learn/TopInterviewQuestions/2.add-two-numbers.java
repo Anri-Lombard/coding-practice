6657aa2b0c75a9572267703be8d15973cf3b1378
Anri Lombard <anri.m.lombard@gmail.com>
2022-03-19 06:36:35 +0200

    Practice Progress

--- Leetcode/Learn/TopInterviewQuestions/2.add-two-numbers.java
+++ Leetcode/Learn/TopInterviewQuestions/2.add-two-numbers.java
@@ -17,12 +17,27 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int len = l1.size() < l2.size() ? l1.size() : l2.size();
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
 
-        ListNode newNode = new ListNode();
-        for (int i = 0; i < len; i++) {
-            
+        while (l1 != null || l2 != null || carry == 1) {
+            int sum = 0;
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
+            carry = sum/10;
+            ListNode node = new ListNode(sum%10);
+            curr.next = node;
+            curr = curr.next;
         }
+        return dummy.next;
     }
 }
 // @lc code=end
